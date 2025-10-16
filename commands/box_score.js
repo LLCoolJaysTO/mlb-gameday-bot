a45649ed83a5c5ae94abdf121abb3db06558326e
LLCoolJaysTO <liamlanger1836@gmail.com>
2025-10-15 23:02:45 -0400

    Update box_score.js

--- commands/box_score.js
+++ commands/box_score.js
@@ -1,20 +1,23 @@
 const interactionHandlers = require('../modules/interaction-handlers.js');
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('box_score')
-        .setDescription('View the current game\'s box score.'),
-    async execute (interaction) {
-        try {
-            await interactionHandlers.boxScoreHandler(interaction);
-        } catch (e) {
-            console.error(e);
-            if (interaction.deferred && !interaction.replied) {
-                await interaction.followUp('There was an error processing this command. If it persists, please reach out to the developer.');
-            } else if (!interaction.replied) {
-                await interaction.reply('There was an error processing this command. If it persists, please reach out to the developer.');
-            }
-        }
+  data: new SlashCommandBuilder()
+    .setName('box_score')
+    .setDescription('View the current game\'s box score.'),
+  async execute(interaction) {
+    try {
+      // 👇 Add this line
+      await interaction.deferReply();
+
+      await interactionHandlers.boxScoreHandler(interaction);
+    } catch (e) {
+      console.error(e);
+      if (interaction.deferred && !interaction.replied) {
+        await interaction.followUp('There was an error processing this command. If it persists, please reach out to the developer.');
+      } else if (!interaction.replied) {
+        await interaction.reply('There was an error processing this command. If it persists, please reach out to the developer.');
+      }
     }
+  }
 };
